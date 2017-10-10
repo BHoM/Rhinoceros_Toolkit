d30506a715404ec7f99bdfc7aa55d9e87afffed5
Eduardo Pignatelli <eduardo.pignatelli@burohappold.com>
2017-10-10 18:08:53 +0100

    Fix Read

--- Rhinoceros_Adapter/CRUD/Read.cs
+++ Rhinoceros_Adapter/CRUD/Read.cs
@@ -4,12 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace BH.Adapter.Rhinoceros
 {
     public partial class RhinocerosAdapter
     {
-        protected override IEnumerable<BHoMObject> Read(Type type, List<object> ids)
+        protected override IEnumerable<BHoMObject> Read(Type type, IList ids)
         {
             throw new NotImplementedException();
         }
