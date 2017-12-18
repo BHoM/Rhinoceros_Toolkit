b53811150ce2be71388188eb39dec23ce74a84a4
epignatel <epignatel@BTH-R90F43Q3>
2017-12-18 12:53:29 +0000

    Added GetExternalEdges

---
+++ Rhinoceros_Engine/Transform/ExternalEdges.cs
@@ -0,0 +1,23 @@
+﻿using BH.Adapter.Rhinoceros;
+using BH.oM.Geometry;
+using RG = Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Engine.Rhinoceros
+{
+    public static partial class Transform
+    {
+        /***************************************************/
+        /**** Public Methods  - Mesh                    ****/
+        /***************************************************/
+
+        public static List<Polyline> GetExternalEdges(this Mesh mesh)
+        {
+            return mesh.ToRhino().GetNakedEdges().Select(crv => crv.ToBHoM()).ToList();
+        }
+    }
+}
