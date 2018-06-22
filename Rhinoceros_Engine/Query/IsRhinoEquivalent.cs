73ede135c069fbd1c56e19f1cf010f32d6568451
adecler <arnaud.declercq@burohappold.com>
2018-06-22 14:17:45 +0800

    Adding IsRhinoEquivalent method

---
+++ Rhinoceros_Engine/Query/IsRhinoEquivalent.cs
@@ -0,0 +1,25 @@
+﻿using BH.oM.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BH.Engine.Rhinoceros
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods  - Mesh                    ****/
+        /***************************************************/
+
+        public static bool IsRhinoEquivalent(Type type)     
+        {
+            if (typeof(IGeometry).IsAssignableFrom(type))
+                return (type != typeof(Extrusion) && type != typeof(Pipe) && type != typeof(Loft));
+            else
+                return false;
+        }
+
+
+        /***************************************************/
+    }
+}
