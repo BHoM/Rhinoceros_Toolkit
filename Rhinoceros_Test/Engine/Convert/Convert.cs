9860b46656203ad88775eedede37f6e75dc4d16d
epignatel <eduardo.pignatelli@burohappold.com>
2018-07-19 15:05:27 +0100

    Started populating test project

---
+++ Rhinoceros_Test/Engine/Convert/Convert.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RHG = Rhino.Geometry;
+using BHG = BH.oM.Geometry;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BH.Engine.Rhinoceros;
+
+namespace BH.Test.Rhinoceros
+{
+    [TestClass]
+    public partial class Convert
+    {
+        /***************************************************/
+        /**** Private Fields                            ****/
+        /***************************************************/
+
+        private Random m_random = new Random();
+
+        private Dictionary<Type, Type> m_CurveTypesConvention = new Dictionary<Type, Type>
+        {
+            { typeof(RHG.Arc), typeof(BHG.Arc) },
+            { typeof(RHG.ArcCurve), typeof(BHG.Arc) },
+            { typeof(RHG.Circle), typeof(BHG.Circle) },
+            { typeof(RHG.Line), typeof(BHG.Line) },
+            { typeof(RHG.LineCurve), typeof(BHG.Line) },
+            { typeof(RHG.NurbsCurve), typeof(BHG.NurbCurve) },
+            { typeof(RHG.PolyCurve), typeof(BHG.PolyCurve) },
+            { typeof(RHG.Polyline), typeof(BHG.Polyline) },
+            { typeof(RHG.PolylineCurve), typeof(BHG.Polyline) },
+        };
+
+        /***************************************************/
+    }
+}
--- Rhinoceros_Test/Engine/Convert/ToBHoM.cs
+++ Rhinoceros_Test/Engine/Convert/ToBHoM.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using RHG = Rhino.Geometry;
 using BHG = BH.oM.Geometry;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BH.Engine.Rhinoceros;
 
 namespace BH.Test.Rhinoceros
 {
-    [TestClass]
     public partial class Convert
     {
         /***************************************************/
@@ -17,9 +17,9 @@ namespace BH.Test.Rhinoceros
         [TestMethod]
         public void Point3dToBHoM()
         {
-            RHG.Point3d rhinoPoint = Engine.Rhinoceros.Create.RandomPoint3d();
+            RHG.Point3d rhinoPoint = Create.RandomPoint3d(m_random);
             BHG.Point bhPoint = Engine.Rhinoceros.Convert.ToBHoM(rhinoPoint);
-            Assert.IsTrue(bhPoint.X == rhinoPoint.X && bhPoint.Y == rhinoPoint.Y && bhPoint.Z == rhinoPoint.Z);
+            Assert.IsTrue(bhPoint.IsEqual(rhinoPoint));
         }
 
         /***************************************************/
@@ -27,9 +27,9 @@ namespace BH.Test.Rhinoceros
         [TestMethod]
         public void Point3fToBHoM()
         {
-            RHG.Point3f rhinoPoint = Engine.Rhinoceros.Create.RandomPoint3f();
+            RHG.Point3f rhinoPoint = Create.RandomPoint3f(m_random);
             BHG.Point bhPoint = Engine.Rhinoceros.Convert.ToBHoM(rhinoPoint);
-            Assert.IsTrue(bhPoint.X == rhinoPoint.X && bhPoint.Y == rhinoPoint.Y && bhPoint.Z == rhinoPoint.Z);
+            Assert.IsTrue(bhPoint.IsEqual(rhinoPoint));
         }
 
         /***************************************************/
@@ -37,9 +37,9 @@ namespace BH.Test.Rhinoceros
         [TestMethod]
         public void ControlPointToBHoM()
         {
-            RHG.ControlPoint rhinoPoint = Engine.Rhinoceros.Create.RandomControlPoint();
+            RHG.ControlPoint rhinoPoint = Create.RandomControlPoint(m_random);
             BHG.Point bhPoint = Engine.Rhinoceros.Convert.ToBHoM(rhinoPoint);
-            Assert.IsTrue(bhPoint.X == rhinoPoint.Location.X && bhPoint.Y == rhinoPoint.Location.Y && bhPoint.Z == rhinoPoint.Location.Z);
+            Assert.IsTrue(bhPoint.IsEqual(rhinoPoint));
         }
 
         /***************************************************/
@@ -47,9 +47,9 @@ namespace BH.Test.Rhinoceros
         [TestMethod]
         public void Vector3dToBHoM()
         {
-            RHG.Vector3d rhVector = Engine.Rhinoceros.Create.RandomVector3d();
+            RHG.Vector3d rhVector = Create.RandomVector3d(m_random);
             BHG.Vector bhVector = Engine.Rhinoceros.Convert.ToBHoM(rhVector);
-            Assert.IsTrue(bhVector.X == rhVector.X && bhVector.Y == rhVector.Y && bhVector.Z == rhVector.Z);
+            Assert.IsTrue(bhVector.IsEqual(rhVector));
         }
 
         /***************************************************/
@@ -57,9 +57,126 @@ namespace BH.Test.Rhinoceros
         [TestMethod]
         public void Vector3fToBHoM()
         {
-            RHG.Vector3f rhVector = Engine.Rhinoceros.Create.RandomVector3f();
+            RHG.Vector3f rhVector = Create.RandomVector3f(m_random);
             BHG.Vector bhVector = Engine.Rhinoceros.Convert.ToBHoM(rhVector);
-            Assert.IsTrue(bhVector.X == rhVector.X && bhVector.Y == rhVector.Y && bhVector.Z == rhVector.Z);
+            Assert.IsTrue(bhVector.IsEqual(rhVector));
+        }
+
+
+        /***************************************************/
+        /**** Public Methods  - Curves                  ****/
+        /***************************************************/
+
+        [TestMethod]
+        public void ArcToBHoM()
+        {
+            RHG.Arc rhArc = Create.RandomArc(m_random);
+            BHG.Arc bhArc = Engine.Rhinoceros.Convert.ToBHoM(rhArc);
+            Assert.IsTrue(bhArc.IsEqual(rhArc));
+        }
+
+        /***************************************************/
+
+        [TestMethod]
+        public void ArcCurveToBHoM()
+        {
+            RHG.ArcCurve rhArc = Create.RandomArcCurve(m_random);
+            BHG.Arc bhArc = Engine.Rhinoceros.Convert.ToBHoM(rhArc) as BHG.Arc;
+            Assert.IsTrue(bhArc.IsEqual(rhArc));
+        }
+
+        /***************************************************/
+
+        [TestMethod]
+        public void CircleToBHoM()
+        {
+            RHG.Circle rhCircle = Create.RandomCircle(m_random);
+            BHG.Circle bhCircle = Engine.Rhinoceros.Convert.ToBHoM(rhCircle);
+            Assert.IsTrue(bhCircle.IsEqual(rhCircle));
+        }
+
+        /***************************************************/
+
+        [TestMethod]
+        public void LineToBHoM()
+        {
+            RHG.Line rhLine = Create.RandomLine(m_random);
+            BHG.Line bhLine = Engine.Rhinoceros.Convert.ToBHoM(rhLine);
+            Assert.IsTrue(bhLine.IsEqual(rhLine));
+        }
+
+        /***************************************************/
+
+        [TestMethod]
+        public void NurbCurveToBHoM()
+        {
+            RHG.NurbsCurve rhCurve = Create.RandomNurbsCurve(m_random);
+            BHG.NurbCurve bhCurve = rhCurve.ToBHoM() as BHG.NurbCurve;
+            Assert.IsTrue(bhCurve.IsEqual(rhCurve));
+
+            // Checking null return
+            rhCurve = null;
+            bhCurve = rhCurve.ToBHoM() as BHG.NurbCurve;
+            Assert.IsTrue(bhCurve.IsEqual(rhCurve));
+        }
+
+        /***************************************************/
+
+        [TestMethod]
+        public void PolyCurveToBHoM()
+        {
+            RHG.PolyCurve rhPolyCurve = Create.RandomPolyCurve(m_random);
+            BHG.PolyCurve bhPolyCurve = rhPolyCurve.ToBHoM() as BHG.PolyCurve;
+
+            rhPolyCurve.RemoveNesting();
+            RHG.Curve[] rhCurves = rhPolyCurve.Explode();
+            List<BHG.ICurve> bhCurves = bhPolyCurve.Curves;
+
+            Assert.IsTrue(rhCurves.Length == bhCurves.Count);
+
+            for (int i = 0; i < bhCurves.Count; i++)
+                Assert.IsTrue(bhCurves[i].IIsEqual(rhCurves[i]));
+        }
+
+        /***************************************************/
+
+        [TestMethod]
+        public void PolylineToBHoM()
+        {
+            RHG.Polyline rhPolyline = Create.RandomPolyline(m_random);
+            BHG.Polyline bhPolyline = rhPolyline.ToBHoM();
+            Assert.IsTrue(bhPolyline.IsEqual(rhPolyline));
+        }
+
+        /***************************************************/
+
+        [TestMethod]
+        public void PolylineCurveToBHoM()
+        {
+            RHG.PolylineCurve rhPolylineCurve = Create.RandomPolylineCurve(m_random);
+            BHG.Polyline bhPolyline = rhPolylineCurve.ToBHoM();
+            Assert.IsTrue(bhPolyline.IsEqual(rhPolylineCurve));
+
+            rhPolylineCurve = null;
+            bhPolyline = rhPolylineCurve.ToBHoM();
+            Assert.IsTrue(bhPolyline.IsEqual(rhPolylineCurve));
+        }
+
+        /***************************************************/
+
+        [TestMethod]
+        public void CurveToBHoM()
+        {
+            List<RHG.Curve> rhCurves = Create.RandomCurves(m_random);
+            List<BHG.ICurve> bhCurves = rhCurves.Select(c => c.ToBHoM()).ToList();
+            bool typeMatches = false;
+            bool geometryMatches = false;
+            for (int i = 0; i < rhCurves.Count; i++)
+            {
+                typeMatches = m_CurveTypesConvention[rhCurves[i].GetType()] == bhCurves[i].GetType();
+                geometryMatches = bhCurves[i].IIsEqual(rhCurves[i]);
+            }
+            Assert.IsTrue(typeMatches && geometryMatches);
         }
 
         /***************************************************/
