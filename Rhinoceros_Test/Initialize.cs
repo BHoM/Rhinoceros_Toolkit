cbdb00ec066524a777d4b58e9eb546aeca80c376
epignatel <eduardo.pignatelli@burohappold.com>
2018-07-23 10:54:19 +0100

    documenting test assembly initialization

--- Rhinoceros_Test/Initialize.cs
+++ Rhinoceros_Test/Initialize.cs
@@ -50,11 +50,26 @@ namespace BH.Test.Rhinoceros
         /**** Private Fields                            ****/
         /***************************************************/
 
+        /// <summary>
+        /// <see cref="https://github.com/BuroHappoldEngineering/Rhinoceros_Toolkit/pull/52#pullrequestreview-138914924"/> 
+        /// From Steve Baer in McNeel:
+        /// The majority of RhinoCommon depends on functionality exported from the rhcommon_c.dll
+        /// The rhcommon_c.dll depends on Rhino.exe
+        /// Due to this dependency chain, Rhino.exe needs to be the host process.
+        /// <see cref="https://discourse.mcneel.com/t/unit-testing-plugin-code/39830"/>
+        /// <see cref="https://discourse.mcneel.com/t/unit-testing-plug-in-code-with-nunit/3367/6"/>
+        /// 
+        /// Once we upgrade to Rhino v7 we can take advantage of:
+        /// <see cref="https://discourse.mcneel.com/t/rhinocommon-unit-testing/61317"/>
+        /// </summary>
         private static string[] m_RequiredFiles = new string[]
         {
-            "Ltdis15x.dll", "Ltfil15x.dll", "Ltkrn15x.dll",
-            "openNURBSx64.dll", "rhcommon_c.dll", "Rhino.exe",
-            "Rhino_DotNet.dll", "RhinoCommon.dll", "tlx64.dll"
+            "Ltdis15x.dll", "Ltfil15x.dll", "Ltkrn15x.dll", // Needed due to the LeadTools framework
+            "openNURBSx64.dll", // Required as standard library of geometrical objects
+            "rhcommon_c.dll", "RhinoCommon.dll", // Actual Rhinoceros SDKs
+            "Rhino.exe", // Rhino host process. Any dll call must come from here.
+            "Rhino_DotNet.dll", // Not sure, but it will be disposed later on.
+            "tlx64.dll" // Not sure, licensing?
         };
     }
 }
