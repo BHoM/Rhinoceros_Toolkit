ef991884ebadca733747438733f9aec436015ee7
Eduardo Pignatelli <Eduardo.Pignatelli@BuroHappold.com>
2019-10-24 11:45:24 +0100

    Using rhino logic to check for disconnected PolyCurve
    To make sure we don't break stuff with nurbs

--- Rhinoceros_Engine/Convert/ToRhino.cs
+++ Rhinoceros_Engine/Convert/ToRhino.cs
@@ -222,19 +222,15 @@ namespace BH.Engine.Rhinoceros
             if (bPolyCurve == null)
                 return null;
 
+            IEnumerable<RHG.Curve> parts = bPolyCurve.Curves.Select(x => x.IToRhino());
+            
             // Check if bPolycurve is made of disconnected segments
-            IEnumerable<BHG.Point> endpoints = bPolyCurve.Curves.Select(x => x.IStartPoint());
-            endpoints = endpoints.Concat(bPolyCurve.Curves.Select(x => x.IEndPoint()));
-            endpoints = endpoints.ToList().CullDuplicates();
-            bool isDisconnected = endpoints.Count() != bPolyCurve.DiscontinuityPoints().Count;
-
-            if (isDisconnected)
-            {
+            if (RHG.Curve.JoinCurves(parts).Length > 1)
                 return null;
-            }
 
             RHG.PolyCurve rPolycurve = new RHG.PolyCurve();
-            bPolyCurve.Curves.ForEach(curve => rPolycurve.Append(curve.IToRhino()));
+            foreach (RHG.Curve curve in parts)
+                rPolycurve.Append(curve);
             return rPolycurve;
         }
 
