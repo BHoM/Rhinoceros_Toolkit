4ab3bf1a42414c70c57c6dc5511e6c0e40fd32b4
rolyh <roly.hudson@gmail.com>
2021-02-25 14:10:19 +0000

    Try again with Adapter_oM ref

--- Rhinoceros_oM/RhinocerosConfig.cs
+++ Rhinoceros_oM/RhinocerosConfig.cs
@@ -1,11 +1,11 @@
-﻿using BH.oM.Adapter;
-using BH.oM.Base;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BH.oM.Adapter;
+using BH.oM.Base;
 
 namespace BH.oM.Adapters.Rhinoceros
 {
