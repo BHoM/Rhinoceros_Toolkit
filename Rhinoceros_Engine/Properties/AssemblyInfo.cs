17c113eee8938aaa73f3c916f7e6ef778c2e2e41
FraserGreenroyd <fraser.greenroyd@burohappold.com>
2022-07-18 17:38:04 +0100

    Update Assembly Information

--- Rhinoceros_Engine/Properties/AssemblyInfo.cs
+++ Rhinoceros_Engine/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Rhinoceros_Engine")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/BHoM/Rhinoceros_Toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Rhinoceros_Engine")]
